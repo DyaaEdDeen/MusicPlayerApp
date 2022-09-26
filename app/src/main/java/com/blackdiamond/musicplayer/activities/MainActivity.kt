087ad6dcc2de78d8207332334bd1bd61988c18bb
DyaaEDeen <dyaaeldeen057@gmail.com>
2022-09-26 16:37:18 +0200

    Created SplashScreen.kt

--- app/src/main/java/com/blackdiamond/musicplayer/activities/MainActivity.kt
+++ app/src/main/java/com/blackdiamond/musicplayer/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.blackdiamond.musicplayer
+package com.blackdiamond.musicplayer.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blackdiamond.musicplayer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
